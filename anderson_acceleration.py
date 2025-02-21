b1527a3f9e56ca8de173aac547335d9d157bcc68
Eric Gelphman <egelphman17@gmail.com>
2025-02-21 14:46:37 -0700

    Modified implementation for increased vectorization

--- anderson_acceleration.py
+++ anderson_acceleration.py
@@ -1,97 +1,143 @@
 # Eric Gelphman
 # Colorado School of Mines Department of Applied Mathematics and Statistics
-# February 13, 2025
-import torch
+# February 20, 2025
 
-def banach(N, T_eval, eps=1.0e-3):
+from collections import deque
+import torch
+import matplotlib.pyplot as plt
+import time
+
+# Random 5-Layer CV Network using CNN for testing
+class RandomLayer(torch.nn.Module):
+
+    def __init__(self, n_channels, n_inner_channels, kernel_size=3, num_groups=8):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(n_channels, n_inner_channels, kernel_size, padding=kernel_size // 2, bias=False)
+        self.conv2 = torch.nn.Conv2d(n_inner_channels, n_channels, kernel_size, padding=kernel_size // 2, bias=False)
+        self.norm1 = torch.nn.GroupNorm(num_groups, n_inner_channels)
+        self.norm2 = torch.nn.GroupNorm(num_groups, n_channels)
+        self.norm3 = torch.nn.GroupNorm(num_groups, n_channels)
+        self.conv1.weight.data.normal_(0, 0.01)
+        self.conv2.weight.data.normal_(0, 0.01)
+
+    def forward(self, x, d):
+        """
+        Forward Pass of network
+        """
+        y = self.norm1(torch.nn.functional.relu(self.conv1(x)))
+        return self.norm3(torch.nn.functional.relu(x + self.norm2(d + self.conv2(y))))
+
+
+def banach(x0, T_eval, data, eps=1.0e-3, max_itr=75):
     """
     Banach Fixed-Point Iteration
 
     Parameters:
-        N: Dimension of input vector
+        x0: Initial Guess
         T_eval: Function to evaluate in fixed point iteration
+        data: Data in NN
         eps: Epsilon for convergence check
+        max_itr: Max. number of iterations
 
     Return:
         Fixed point of T_eval
     """
-    x_k = torch.zeros(N)
-    x_k1 = T_eval(x_k)
+    x_k = x0
+    x_k1 = T_eval(x0, data)
     k = 0
-    while torch.linalg.vector_norm(x_k - x_k1) > eps:
+    res = []
+    res_k = (x_k1 - x_k).norm().item() / (1.0e-9 + x_k1.norm().item())
+    while (res_k > eps and k < max_itr):
+        res.append(res_k)
         x_k = x_k1
-        x_k1 = T_eval(x_k)
+        x_k1 = T_eval(x_k, data)
         k += 1
-    return x_k, k
+        res_k = (x_k1 - x_k).norm().item() / (1.0e-9 + x_k1.norm().item())
+
+    return x_k, k, res
 
 def f(x):
     return 8.0 + 0.4*torch.sin(2.0*x)
 
-def anderson(N, T_eval, beta = 0.5, m=5, lam=1.0e-6, eps=1.0e-3):
+def anderson(x0, T_eval, data, m=5, beta = 0.5, lam=1.0e-6, eps=1.0e-3, max_itr=75):
     """
     Fixed-Point Iteration with Anderson acceleration 
 
     Parameters:
-        N: Dimension of input vector
+        x0: Initial guess
         T_eval: Function to evaluate in fixed point iteration
-        beta: Parameter in Anderson acceleration iteration
+        data: Data in NN
         m: Number of previous iterations to use in least-squares 
            optimization problem
+        beta: Parameter in Anderson acceleration iteration
         lam: Regularization parameter
         eps: Epsilon for convergence check
 
     Return:
         Fixed point of T_eval
     """
-    x_hist = []#Stack of previous x_k values, highest index is most recent
-    G = []#Stack of previous residual T_eval(x_k) - x_k values, highest index is most recent
-    T_eval_hist = []#Stack of previous T_eval(x_k) values, highest index is most recent
-    x_hist.append(torch.zeros(N))
-    G.append(T_eval(x_hist[0]) - x_hist[0])
-    k = 0
-    T_eval_hist.append(T_eval(x_hist[0]))
-    while torch.linalg.vector_norm(x_hist[-1] - T_eval_hist[-1]) > eps:
-        if k < m-1:
-            #Update x_k using Banach fixed point iteration
-            x_k = T_eval(x_hist[-1])
-            T_k = T_eval(x_k)
-            #Update history stacks
-            x_hist.append(x_k)
-            T_eval_hist.append(T_k)
-            G.append(T_k - x_k)
-        else:
-            #Solve least-squares problem
-            H = torch.ones((m+1,m+1))
-            H[0,0] = 0
-            GG = torch.stack(G,dim=1)
-            y = torch.zeros(m+1)
-            y[0] = 1.0
-            H[1:,1:] = (GG.T)@GG + lam*torch.eye(m)
-            alpha = torch.linalg.solve(H,y)[1:]
-            #Update x_k
-            s1 = torch.zeros(N)
-            s2 = torch.zeros(N)
-            for i in range(1,m+1):
-                s1 += (alpha[i-1].item())*x_hist[-i]
-                s2 += (alpha[i-1].item())*T_eval_hist[-i]
-            x_k = (1.0 - beta)*s1 + beta*s2
-            T_k = T_eval(x_k)
-            #Update history stacks
-            x_hist.append(x_k)
-            T_eval_hist.append(T_k)
-            G.append(T_k - x_k)
-            x_ = x_hist.pop(0)
-            T_ = T_eval_hist.pop(0)
-            G_ = G.pop(0)
-        k+=1
+    batch_sz, d, h, w = x0.shape
+    x_hist = torch.zeros(batch_sz, m, d*h*w, dtype=x0.dtype, device=x0.device)
+    T_eval_hist = torch.zeros(batch_sz, m, d*h*w, dtype=x0.dtype, device=x0.device)
+    x_hist[:,0] = x0.view(batch_sz, -1)
+    T_eval_hist[:,0] = T_eval(x0, data).view(batch_sz,-1)
+    x_hist[:,1] = T_eval_hist[:,0]
+    T_eval_hist[:,1] = T_eval(T_eval_hist[:,0].view_as(x0), data).view(batch_sz,-1)
+    H = torch.zeros(batch_sz, m+1, m+1, dtype=x0.dtype, device=x0.device)
+    H[:,0,1:] = 1
+    H[:,1:,0] = 1
+    Batch_RHS = torch.zeros(batch_sz, m+1, 1, dtype=x0.dtype, device=x0.device)
+    Batch_RHS[:,0] = 1 
 
-    return x_hist[-1], k
+    res = []
+    res_k = ((T_eval_hist[:,0] - x_hist[:,0]).norm().item()) / (1.0e-9 + T_eval_hist[:,0].norm().item())
+    res.append(res_k)
+    k = 1
+    res_k = ((T_eval_hist[:,k%m] - x_hist[:,k%m]).norm().item()) / (1.0e-9 + T_eval_hist[:,k%m].norm().item())
+    res.append(res_k)
+    k += 1
+    while (res_k > eps and k < max_itr):
+        M = min(k,m)
+        G = T_eval_hist[:,:M] - x_hist[:,:M]
+        H[:,1:(M+1),1:(M+1)] = torch.bmm(G, G.transpose(1,2)) + lam*torch.eye(M, dtype=x0.dtype, device=x0.device)[None]
+
+        #Solve for alpha
+        alpha = None
+        try:
+            alpha = torch.linalg.solve(H[:,:(M+1),:(M+1)], Batch_RHS[:,:(M+1)])[:,1:(M+1),0]#Result is batch_sz x n
+        except RuntimeError:#If matrix is singular solve using QR least squares
+            alpha = torch.linalg.lstsq(H[:,:(M+1),:(M+1)], Batch_RHS[:,:(M+1)])[0][:,1:(M+1)]
+
+        #Update data structures
+        x_hist[:,k%m] = (1-beta)*((alpha[:,None]@x_hist[:,:M])[:,0]) + beta*((alpha[:,None]@T_eval_hist[:,:M])[:,0])
+        T_eval_hist[:,k%m] = T_eval(x_hist[:,k%m].view_as(x0), data).view(batch_sz, -1)
+        res_k = ((T_eval_hist[:,k%m] - x_hist[:,k%m]).norm().item()) / (1.0e-9 + T_eval_hist[:,k%m].norm().item())
+        res.append(res_k)
+        k += 1
+
+    return x_hist[:,k%m].view_as(x0), k, res
 
 def main():
-    b_f = banach(25, f, eps=1.0e-4)
-    print(b_f)
-    a_f = anderson(25, f, eps=1.0e-4)
-    print(a_f)
+    batch_size = 100
+    X = 100.0*torch.randn(batch_size,64,32,32)
+    R_NN = RandomLayer(64,128)
+
+    b_start = time.time()
+    b_fixed_pt, b_itr, res_b = banach(torch.zeros_like(X), R_NN.forward, X, eps=1.0e-3)
+    b_end = time.time()
+    print("Execution time for Banach: " + str(b_end - b_start) + "   Number of Banach iterations: " + str(b_itr))
+    a_start = time.time()
+    a_fixed_pt, a_itr, res_a = anderson(torch.zeros_like(X), R_NN.forward, X, beta=1.5, eps=1.0e-3)
+    a_end = time.time()
+    print("Execution time for Anderson: " + str(a_end - a_start) + "   Number of Anderson iterations: " + str(a_itr))
+    plt.figure()
+    plt.semilogy(range(b_itr), res_b, label="Banach", color='b')
+    plt.semilogy(range(a_itr), res_a, label="Anderson", color='g')
+    plt.xlabel("Iteration")
+    plt.ylabel("Relative residual")
+    plt.title("Batch Size: " + str(batch_size))
+    plt.legend()
+    plt.show()
 
 if __name__ == "__main__":
     main()
